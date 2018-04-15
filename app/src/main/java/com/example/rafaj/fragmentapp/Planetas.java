a05d2afaff7d651d19ff11db753d1e8cb0e574ef
Meight <7ace.lex@gmail.com>
2018-04-15 10:14:03 -0600

    preparando image

--- app/src/main/java/com/example/rafaj/fragmentapp/Planetas.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/Planetas.java
@@ -10,12 +10,12 @@ import java.util.HashMap;
 public class Planetas implements Serializable{
 
     private String titulo;
-    private HashMap<Integer, Integer> imageViewHashMap;
+    private HashMap<Integer, Integer> imageHashMap;
     private int indice, imageResource;
 
 
     public Planetas(){
-        imageViewHashMap = new HashMap<>();
+        imageHashMap = new HashMap<>();
     }
 
     /*private void init(){
@@ -37,25 +37,23 @@ public class Planetas implements Serializable{
     }
 
 
-    /**
-     * @return the value of the planet image id
-     */
+
     public int getImageView(){
-        return imageViewHashMap.get(indice);
+        return imageHashMap.get(indice);
     }
 
 
-    /*private void setArrayID(){
-        imageViewHashMap.put(0, R.drawable.sun_0);
-        imageViewHashMap.put(1, R.drawable.mercury_1);
-        imageViewHashMap.put(2, R.drawable.venus_2);
-        imageViewHashMap.put(3, R.drawable.earth_3);
-        imageViewHashMap.put(4, R.drawable.mars_4);
-        imageViewHashMap.put(5, R.drawable.jupiter_5);
-        imageViewHashMap.put(6, R.drawable.saturn_6);
-        imageViewHashMap.put(7, R.drawable.uranos_7);
-        imageViewHashMap.put(8, R.drawable.neptune_8);
-    }*/
+    private void setImage(){
+        imageHashMap.put(0, R.drawable.sol);
+        imageHashMap.put(1, R.drawable.mercurio);
+        imageHashMap.put(2, R.drawable.venus);
+        imageHashMap.put(3, R.drawable.tierra);
+        imageHashMap.put(4, R.drawable.marte);
+        imageHashMap.put(5, R.drawable.jupiter);
+        imageHashMap.put(6, R.drawable.saturno);
+        imageHashMap.put(7, R.drawable.urano);
+        imageHashMap.put(8, R.drawable.neptuno);
+    }
 
 
 
@@ -75,22 +73,7 @@ public class Planetas implements Serializable{
     }
 
 
-    /**
-     * @return the value of the color id by planet
-     * @author Pedro Gómez
-     */
-    /*public int getColor(){
-        switch (indice){
-            case 0: return R.color.Sol;
-            case 1: return R.color.Mercurio;
-            case 2: return R.color.Venus;
-            case 3: return R.color.Earth;
-            case 4: return R.color.Mars;
-            case 5: return R.color.Jupiter;
-            case 6: return R.color.Saturn;
-            case 7: return R.color.Uranus;
-            case 8: return R.color.Neptune;
-        }
-        return 10;
-    }*/
+
+
+
 }
